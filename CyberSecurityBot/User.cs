4a523c3c7fc5193944d6797590794685a16b51e8
nazKIyani <Kiyaninazeera4@gmail.com>
2026-03-31 22:10:56 +0200

    Added ASCII image display

--- CyberSecurityBot/User.cs
+++ CyberSecurityBot/User.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CyberSecurityBot
 {
-    internal class Class2
+    public class User
     {
+        public string Name { get; set; } = "Guest";
+
+        // Simple validation for Part 1 POE
+        public bool IsValidName(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input) && input.Length >= 2;
+        }
     }
 }
